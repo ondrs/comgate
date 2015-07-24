c5cd586927637c981cf6328c1e52777a2b4d99c5
ondrs <plsek.o@gmail.com>
2015-07-24 15:45:22 +0200

    throw an exception in a case of an error

--- src/ondrs/Comgate/AgmoPaymentsSimpleProtocol.php
+++ src/ondrs/Comgate/AgmoPaymentsSimpleProtocol.php
@@ -109,6 +109,11 @@ class AgmoPaymentsSimpleProtocol {
         curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
         $responseBody = curl_exec($c);
         curl_close($c);
+
+        if(!$responseBody) {
+            throw new Exception(curl_error($c), curl_errno($c));
+        }
+
         return $responseBody;
     }
 
